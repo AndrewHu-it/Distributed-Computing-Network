69209d6cf45f281b9cdaf657d2e854cc62db7894
andrew <ahurlbut999@gmail.com>
2025-01-29 07:09:41 -0800

    Added app Icon

--- Distributed-Computing-Network/Distributed_Computing_NetworkApp.swift
+++ Distributed-Computing-Network/Distributed_Computing_NetworkApp.swift
@@ -1,14 +1,9 @@
-//
-//  Distributed_Computing_NetworkApp.swift
-//  Distributed-Computing-Network
-//
-//  Created by Andrew Hurlbut on 1/28/25.
-//
-
 import SwiftUI
 
 @main
 struct Distributed_Computing_NetworkApp: App {
+    // Define your property here
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
